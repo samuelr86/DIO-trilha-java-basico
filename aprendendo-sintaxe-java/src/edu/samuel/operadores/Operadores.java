a1eaa455f0c9321410d745d5c050721ff075b1a7
Samuel Razzaboni <samuel.razzaboni@gmail.com>
2022-10-15 19:08:01 -0300

    Operadores

---
+++ aprendendo-sintaxe-java/src/edu/samuel/operadores/Operadores.java
@@ -0,0 +1,99 @@
+import java.util.Date;
+
+public class Operadores{
+    public static void main(String[] args) {
+        
+
+        // USO DO OPERADOR DE ATRIBUIÇÃO
+
+        String nome = "Samuel";
+        nome = "Samuel Razz";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = true;
+        Date dataNascimento = new Date();
+
+        // OPERADORES ARITMÉTICOS
+
+        double soma = 10.5 + 15.7;
+        int subtracao = 113-25;
+        int multiplicacao = 20*7;
+        double divisao = 15/2;
+        int modulo = 18%3;
+
+        // concatenação
+        String name = "John" + "Wick";
+
+        // UNÁRIO
+
+        int numero = 5; // positivo
+        numero = -numero; // ele mesmo negativo
+
+        numero = numero * -1; //volta ser positivo
+
+        //incremento
+        numero++; //5+1= 6
+
+        //decremento
+        numero--; //6-1= 5
+
+        doadorOrgao = !doadorOrgao; // contrário true, ou false.
+
+        // TERNÁRIO
+
+        int a = 5;
+        int b = 6;
+
+        if (a == b){
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+        /*Pode ser escrito assim: */
+
+        boolean resultado = (a == b)?true:false;
+
+        // RELACIONAIS
+
+        int numero1 = 2;
+        int numero2 = 5;
+
+        boolean simNao = numero1 == numero2; //igual
+        simNao = numero1 != numero2; //diferente
+        simNao = numero1 >= numero2; // maior ou igual a
+        simNao = numero1 <= numero2; // menor ou igual a
+        simNao = numero1 > numero2; // maior
+        simNao = numero1 < numero2; // menor
+
+        // LÓGICOS
+
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if (condicao1 && condicao2) {
+            System.out.println("As duas sao verdadeiras");
+        }
+
+        if (condicao1 || condicao2) {
+            System.out.println("uma das duas é verdadeira");
+        }
+
+        condicao2 = false;
+
+        if (condicao1 && condicao2) {
+            System.out.println("Uma delas é falsa, estão essa relação é falsa.");
+        }
+
+        if (condicao1 || condicao2) {
+            System.out.println(" Uma das condiçoes é falsa");
+        }
+
+
+
+
+
+
+    }
+}
---
+++ aprendendo-sintaxe-java/src/edu/samuel/operadores/README.md
@@ -0,0 +1,57 @@
+## Operadores
+
+### Atribuição
+Representado pelo simbolo de igualdade "=".
+Utilizado para colocar um valor inicial para uma variável ou alterar esse valor.
+
+### Aritméticos
+
+São utilizados para realizar operações matemáticas entre valores numéricos, são eles:
+ - + (adição)
+ - - (Subtração)
+ - * (multiplicação)
+ - / (divisão)
+ - % (módulo) verifica se a divisão resulta em números fracionados.
+
+Obs.: quando usado o simbolo de + entre textos ele realizará a "concatenação de texto" que é a composição de palavras
+
+### Unários
+
+São aplicados juntamente com outro operador aritmético. Realizam o trabalho de incrementar, decrementar ou inverter valores numéricos e booleanos.
+ - positivo(+): números sao positivos explicitamente sem esse operador
+ - negativo(-): nega um número ou expressão aritmética
+ - incremento (++): incrementa o valor de 1 unidade
+ - decremento (--): decrementa o valor de 1 unidade
+ - lógico de negação (!): nega o valor booleano de uma expressão
+
+### Ternário
+
+É uma forma resumida para definir uma condição e escolher por um dentre dois valores. Deve-se pensar como um 'IF' normal escrito em uma única linha.
+É representado pelos símbolos ':' e '?', utilizados na seguinte estrutura de sintaxe:
+
+<condição booleana> ? <verdadeiro> : <falso>
+
+### Relacionais
+
+São operadores que avaliam a relação entre duas variáveis, se elas são iguais (==), diferentes(!=), maior(>), maior ou igual a (>=), menor(<) e menor ou igual a(<=).
+
+### Lógicos
+
+Representam o recurso que nos permite criar expressões lógicas.
+são os símbolos: && (and) e  || (or) utilizados na tabela verdade
+
+&&
+V + V = V
+F + F = F
+V + F = F
+F + V = F
+
+||
+V + V = V
+F + F = F
+V + F = V
+F + V = V
+
+
+
+
